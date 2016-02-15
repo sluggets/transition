46fdfec3b415c09743ab02dcd0ee49e2ea208e62
timothy <tim.longoria@gmail.com>
2016-02-15 09:34:29 -0600

    Added profile footer

--- footer.php
+++ footer.php
@@ -1,6 +1,19 @@
-<footer>
-  <div class="footer_text">
-    <p>See <a href="sources.php">Sources</a> for reference material</p>
-    <p>Developed by Timothy Longoria as a practice exercise.</p>
-  </div>
-</footer>
+    <div class="row">
+      <div class="col-xs-12 col-md-12 col-lg-12">
+        <footer>
+          <ul class="list-group">
+            <li id="ti"><h3 class="list-group-item-heading"><a href="javascript:void(0)">Timothy Longoria</a></h3><img id="timpic" class="img-responsive" src="http://media.timlongoria.com/t150.jpg"></li>
+            <li id="ab"><h3 class="list-group-item-heading"><a href="javascript:void(0)">About</a></h3><p id="about" class="list-group-item-text">Self-educated school bus driver, motivated to keep learning!</p></li>
+            <li id="pr">
+              <h3 class="list-group-item-heading"><a href="javascript:void(0)">Projects</a></h3>
+                <p id="projects" class="list-group-item-text">
+                  <a href="http://calculator.timlongoria.com" target="_blank">Calculonator<i class="fa fa-external-link"></i></a><br>
+                  <a href="http://wordfib.timlongoria.com" target="_blank">Wordfib<i class="fa fa-external-link"></i></a><br>
+                  <a href="http://ocarinaquotes.timlongoria.com" target="_blank">Ocarina of Time Quote Generator<i class="fa fa-external-link"></i></a><br>
+              </p>
+            </li>
+            <li><h3 class="list-group-item-heading"><a href="http://portfolio.timlongoria.com" target="_blank">Portfolio<i class="fa fa-external-link"></i></a></h3></li>
+          </ul>
+        </footer>
+      </div>
+    </div>
--- js/jquery.script.js
+++ js/jquery.script.js
@@ -266,4 +266,16 @@ $(document).ready(function() {
         $('#chev_down_human').toggle();
     });
 
+  // mini portfolio navigation
+    $('#ab').click(function(){
+      $('#about').toggle(600);
+    });
+ 
+    $('#pr').click(function(){
+      $('#projects').toggle(600);
+    });
+
+    $('#ti').click(function(){
+      $('#timpic').toggle(600);
+    });
 });
